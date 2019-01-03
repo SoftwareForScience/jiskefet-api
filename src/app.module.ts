1f774ecc2e7309ae70143020bbec79e550ded09c
CalvinHuynh <huynhck001@gmail.com>
2019-01-03 23:02:16 +0100

    typeorm config is now being read from the .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -61,7 +61,19 @@ if (process.env.NODE_ENV === 'test') {
         migrationsRun: true
     };
 } else {
-    databaseOptions = defaultDatabaseOptions;
+    databaseOptions = {
+      type: process.env.TYPEORM_CONNECTION,
+      host: process.env.TYPEORM_HOST,
+      port: process.env.TYPEORM_PORT,
+      username: process.env.TYPEORM_USERNAME,
+      password: process.env.TYPEORM_PASSWORD,
+      database: process.env.TYPEORM_DATABASE,
+      entities: [process.env.TYPEORM_ENTITIES],
+      logging: process.env.TYPEORM_LOGGING,
+      synchronize: process.env.TYPEORM_SYNCHRONIZE ? true : false,
+      migrations: [process.env.TYPEORM_MIGRATIONS]
+      // what to do with the cli variable from ormcofnig.json
+    };
 }
 
 const authServiceProvider = {
