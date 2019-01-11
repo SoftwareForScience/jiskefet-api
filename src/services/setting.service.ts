504efcb2497a24c4614db093f76ce579a262ba34
CalvinHuynh <huynhck001@gmail.com>
2019-01-11 23:20:09 +0100

    updated setting service

--- src/services/setting.service.ts
+++ src/services/setting.service.ts
@@ -12,20 +12,24 @@ import { Setting } from '../interfaces/setting.interface';
 @Injectable()
 export class SettingService {
 
-    async getSettings(): Promise<Setting> {
+    async getSettings(): Promise<{ data: Setting }> {
         if (process.env.USE_CERN_SSO === 'true') {
             return {
-                ['USE_CERN_SSO']: process.env.USE_CERN_SSO,
-                ['AUTH_URL']:
-                    `https://oauth.web.cern.ch/OAuth/Authorize?response_type=code&client_id=${ process.env.CLIENT_ID }&
-                    redirect_uri=${ process.env.AUTH_REDIRECT_URI }`
+                data: {
+                    ['USE_CERN_SSO']: process.env.USE_CERN_SSO,
+                    ['AUTH_URL']:
+                        `https://oauth.web.cern.ch/OAuth/Authorize?response_type=code`
+                        + `&client_id=${process.env.CLIENT_ID}&redirect_uri=${ process.env.AUTH_REDIRECT_URI}`
+                }
             };
         }
         return {
-            ['USE_CERN_SSO']: process.env.USE_CERN_SSO,
-            ['AUTH_URL']:
-                    `https://github.com/login/oauth/authorize?response_type=code&client_id=${ process.env.CLIENT_ID }&`
-                    + `redirect_uri=${ process.env.AUTH_REDIRECT_URI }`,
+            data: {
+                ['USE_CERN_SSO']: process.env.USE_CERN_SSO,
+                ['AUTH_URL']:
+                    `https://github.com/login/oauth/authorize?response_type=code&client_id=${process.env.CLIENT_ID}&`
+                    + `redirect_uri=${process.env.AUTH_REDIRECT_URI}`,
+            }
         };
     }
 }
