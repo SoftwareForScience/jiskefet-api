d9bffbd5d68ae44231b8ea36baf2c85e7f063c2a
CalvinHuynh <huynhck001@gmail.com>
2019-03-22 15:20:14 +0100

    FLP: added dto for patch endpoint, started implementing service logic

--- src/controllers/flp.controller.ts
+++ src/controllers/flp.controller.ts
@@ -14,6 +14,7 @@ import { FlpRole } from '../entities/flp_role.entity';
 import { CreateFlpDto } from '../dtos/create.flp.dto';
 import { FlpSerivce } from '../services/flp.service';
 import { createResponseItem, createErrorResponse } from '../helpers/response.helper';
+import { PatchFlpDto } from '../dtos/patch.flp.dto';
 
 @ApiUseTags('flp')
 @ApiBearerAuth()
@@ -29,11 +30,16 @@ export class FlpController {
      * @param id unique identifier for a Flp.
      * @param name of FLP.
      */
-    @Get(':id')
-    async findById(@Param('id') runId: number, flpName: string): Promise<ResponseObject<FlpRole>> {
+    @Get(':name/runs/:id')
+    async findById(@Param('id') runId: number, @Param('name') flpName: string): Promise<ResponseObject<FlpRole>> {
         // Aggegrate readout bytes so it can be updated for the run table,
         // aswell as the number of timeframes and subtimeframes.
-        throw new HttpException(`Endpoint is not yet implemented.`, HttpStatus.NOT_IMPLEMENTED);
+        // throw new HttpException(`Endpoint is not yet implemented.`, HttpStatus.NOT_IMPLEMENTED);
+        try {
+            return createResponseItem(await this.flpService.findOne(flpName, runId));
+        } catch (error) {
+            return createErrorResponse(error);
+        }
     }
 
     /**
@@ -51,13 +57,22 @@ export class FlpController {
     }
 
     /**
-     * During
-     * @param runId unique identifier for updating flp
-     * @param flpName name of a FLP.
+     * Endpoint to update FLP values during a run
+     * @param runId run number
+     * @param flpName flp name
+     * @param request fields to update
      */
-    @Patch(':id')
-    async updateById(@Param(':id') runId: number, flpName: string): Promise<ResponseObject<void>> {
+    @Patch(':name/runs/:id')
+    async updateById(
+        @Param('id') runId: number,
+        @Param('name') flpName: string,
+        @Body() request: PatchFlpDto): Promise<ResponseObject<void>> {
         // Update bytes that are produced for the flp (during runtime).
-        throw new HttpException(`Endpoint is not yet implemented.`, HttpStatus.NOT_IMPLEMENTED);
+        try {
+            const flp = await this.flpService.patch(flpName, runId, request);
+            return createResponseItem(flp);
+        } catch (error) {
+            return createErrorResponse(error);
+        }
     }
 }
--- src/services/flp.service.ts
+++ src/services/flp.service.ts
@@ -13,7 +13,7 @@ import { CreateFlpDto } from '../dtos/create.flp.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { plainToClass } from 'class-transformer';
 import { Run } from '../entities/run.entity';
-import { RunRepository } from '../../test/mocks/run.repository';
+import { PatchFlpDto } from '../dtos/patch.flp.dto';
 
 @Injectable()
 export class FlpSerivce {
@@ -28,6 +28,21 @@ export class FlpSerivce {
         this.runRepository = runRepository;
     }
 
+    async findOne(flpName: string, runNumber: number): Promise<FlpRole> {
+        const run = await this.runRepository.findOne(runNumber);
+        if (!run) {
+            throw new HttpException(`Run with run number ${runNumber} does not exist.`, HttpStatus.NOT_FOUND);
+        }
+
+        return await this.flpRepository
+            .createQueryBuilder('flp_role')
+            .where('flp_hostname = :flpName', {flpName})
+            .andWhere('fk_run_number = :runNumber', {runNumber})
+            .getOne()
+            .then((res: FlpRole) => Promise.resolve(res))
+            .catch((err: string) => Promise.reject(err));
+    }
+
     async create(createFlpDto: CreateFlpDto): Promise<FlpRole> {
         const flpEntity = plainToClass(FlpRole, createFlpDto);
         const run = await this.runRepository.findOne(createFlpDto.run);
@@ -36,5 +51,19 @@ export class FlpSerivce {
         }
         flpEntity.run = run;
         return await this.flpRepository.save(flpEntity);
+    }
+
+    async patch(flpName: string, runNumber: number, patchFlpDto: PatchFlpDto): Promise<FlpRole> {
+        let flpToUpdate: FlpRole;
+        console.log(patchFlpDto);
+        flpToUpdate = await this.findOne(flpName, runNumber);
+        console.log(await flpToUpdate);
+
+        flpToUpdate.nSubTimeframes = patchFlpDto.nSubTimeframes;
+        flpToUpdate.equipmentBytes = patchFlpDto.equipmentBytes;
+        flpToUpdate.recordingBytes = patchFlpDto.recordingBytes;
+        flpToUpdate.fairMQBytes = patchFlpDto.fairMQBytes;
+
+        return await this.flpRepository.save(flpToUpdate);
     }
 }
