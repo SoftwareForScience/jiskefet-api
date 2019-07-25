33a3c1a7dd4aa1a2cb46bc806b840ea46d468dc9
Mats Otten <mats@kepsi.nl>
2019-07-25 13:38:56 +0200

    Create anonymous account

--- src/migration/1539095419272-InitialCreateSchema.ts
+++ src/migration/1539095419272-InitialCreateSchema.ts
@@ -49,6 +49,8 @@ export class InitialCreateSchema1539095419272 implements MigrationInterface {
         await queryRunner.query("ALTER TABLE `tags_in_log` ADD CONSTRAINT `FK_d7e79e3c1d357ef06cb6c76475e` FOREIGN KEY (`fk_tag_id`) REFERENCES `tag`(`tag_id`) ON DELETE CASCADE");
         await queryRunner.query("ALTER TABLE `runs_in_log` ADD CONSTRAINT `FK_be55c390ca8635a3b9dd9260c94` FOREIGN KEY (`fk_log_id`) REFERENCES `log`(`log_id`) ON DELETE CASCADE");
         await queryRunner.query("ALTER TABLE `runs_in_log` ADD CONSTRAINT `FK_2995c0c47c4a8759e48fed93d83` FOREIGN KEY (`fk_run_number`) REFERENCES `run`(`run_number`) ON DELETE CASCADE");
+
+        await queryRunner.query("INSERT INTO `user` (`user_id`, `external_id`, `sams_id`, `name`) VALUES (1,1,1,'Anonymous');");
     }
 
     public async down(queryRunner: QueryRunner): Promise<any> {
